ed1616ab7e41d88783a938779cb8b0884608671d
Degunx <dennygunawan99@gmail.com>
2023-03-23 15:52:47 +0700

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Learning Git v.10</title>
+</head>
+<body>
+    <h1>anda sudah membuat page kosong dengan menggunakan Git</h1>
+</body>
+</html>
