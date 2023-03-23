5bee7e8a9d2feb3858461c5b99f71ba1f35cd577
Degunx <dennygunawan99@gmail.com>
2023-03-23 16:09:16 +0700

    mengubah index.php, menambahkan script dan mengubah style

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
     <title>Learning Git v.10</title>
     <link rel="stylesheet" type = "text/css" href = "css_file.css">
 </head>
-<body>
+<body class ="d-flex">
     <h1>Welcome to HTML page 101</h1>
+    <div class = "d-flex"> 
+        <span id = "show_text"></span>
+        <br>
+        <textarea name="" id="text_input" cols="30" rows="10" placeholder="input your text here"></textarea>
+    </div>
 </body>
+<script src="text/javascript" src="script.js"></script>
 </html>
