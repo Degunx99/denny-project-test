2249bfb4fa7ad872a8a39caaae3efe828503d13d
Degunx <dennygunawan99@gmail.com>
2023-03-24 16:09:17 +0700

    mengubah file index.php

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <link rel="stylesheet" type = "text/css" href = "css_file.css">
 </head>
 <body class ="d-flex">
-    <h1>Welcome to HTML page 101</h1>
+    <h3>Welcome to HTML page 202</h3>
     <div class = "d-flex"> 
         <span id = "show_text"></span>
         <br>
         <textarea name="" id="text_input" cols="30" rows="10" placeholder="input your text here"></textarea>
     </div>
+    <p>contoh paragraf</p>
 </body>
 <script src="text/javascript" src="script.js"></script>
 </html>
